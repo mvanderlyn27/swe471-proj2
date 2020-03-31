76630ad13319906727cb8ebc690da625a5e187f8
FancyPanda <mvanderlyn27@gmail.com>
2020-03-31 15:39:45 -0400

    Fixed bug

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -147,7 +147,7 @@ private void PrintBody (PrintWriter out, String lhs, String rhs, String rslt)
    out.println(" <br>");
    out.println(" <input type=\"submit\" value=\"" + OperationAdd + "\" name=\"Operation\">");
    out.println(" <input type=\"submit\" value=\"" + OperationSub + "\" name=\"Operation\">");
-   out.println(" <input type=\"submit\" value=\""+ OperationMult + "\" name=\Operation\">")
+   out.println(" <input type=\"submit\" value=\""+ OperationMult + "\" name=\Operation\">");
    out.println(" <input type=\"reset\" value=\"Reset\" name=\"reset\">");
    out.println("</form>");
    out.println("");
