e8f6827fd52a473282d97d8a0469a8d5ebfb10fa
FancyPanda <mvanderlyn27@gmail.com>
2020-04-14 04:52:53 -0400

    haha

--- src/main/java/servlet/CustomServer.java
+++ src/main/java/servlet/CustomServer.java
@@ -12,11 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet",
-        urlPatterns = {"/"}
+        urlPatterns = {"/server"}
     )
 
-public class HelloServlet extends HttpServlet
+public class CustomServer extends HttpServlet
 {
+  /**
+   *
+   */
+  private static final long serialVersionUID = 1L;
+
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
     
